fa8e126a2c1f8ba8c6bf2af4bd9d9e8aead34f02
KMB <burduninkonstantin@gmail.com>
2021-04-17 20:38:38 +0300

    log transactions

--- src/main/java/main/controller/ExchangeController.java
+++ src/main/java/main/controller/ExchangeController.java
@@ -3,6 +3,8 @@ package main.controller;
 import main.api.request.ExchangeRequest;
 import main.api.response.ExchangeResponse;
 import main.service.ExchangeService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExchangeController {
 
     private final ExchangeService exchangeService;
+
 
     public ExchangeController(final ExchangeService exchangeService) {
         this.exchangeService = exchangeService;
--- src/main/java/main/service/ExchangeService.java
+++ src/main/java/main/service/ExchangeService.java
@@ -7,6 +7,10 @@ import main.model.Transaction;
 import main.model.User;
 import main.repository.TransactionRepository;
 import main.repository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +27,8 @@ public class ExchangeService {
     private final UserRepository userRepository;
     private final ExchangeRateService exchangeRateService;
     private final TransactionRepository transactionRepository;
+    private final Logger logger;
+    private final Marker transactionMarker;
 
     @Autowired
     public ExchangeService(
@@ -32,6 +38,8 @@ public class ExchangeService {
         this.userRepository = userRepository;
         this.exchangeRateService = exchangeRateService;
         this.transactionRepository = transactionRepository;
+        this.logger = LogManager.getLogger("Exchange");
+        this.transactionMarker = MarkerManager.getMarker("TRANSACTION");
     }
 
     public ResponseEntity<ExchangeResponse> getExchangeResponse(final ExchangeRequest exchangeRequest) {
@@ -39,10 +47,17 @@ public class ExchangeService {
         String to = exchangeRequest.getToCurrency();
         long userId = exchangeRequest.getUserId();
         boolean buyCurrency = from.equals(Currency.RUB.toString());
-        BigDecimal amount = BigDecimal.valueOf(Double.parseDouble(exchangeRequest.getAmount()));
-        BigDecimal resultAmount = buyCurrency ?
-                amount.divide(exchangeRateService.getExchangeRate(to), 2, RoundingMode.HALF_UP)
-                : amount.multiply(exchangeRateService.getExchangeRate(from)).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal amount = BigDecimal.valueOf(Double.parseDouble(exchangeRequest.getAmount())).setScale(2);
+        BigDecimal rate;
+        BigDecimal resultAmount;
+        if (buyCurrency) {
+            rate = exchangeRateService.getExchangeRate(to);
+            resultAmount = amount.divide(rate, 2, RoundingMode.HALF_UP);
+        } else {
+            rate = exchangeRateService.getExchangeRate(from);
+            resultAmount = amount.multiply(rate).setScale(2, RoundingMode.HALF_UP);
+
+        }
 
         long longResultInCents = resultAmount.multiply(BigDecimal.TEN).multiply(BigDecimal.TEN).longValue();
 
@@ -54,16 +69,29 @@ public class ExchangeService {
         transaction.setUser(user);
         transaction.setCurrencyFrom(Currency.valueOf(from));
         transaction.setCurrencyTo(Currency.valueOf(to));
-        transaction.setAmountInCents(amount.multiply(BigDecimal.TEN).multiply(BigDecimal.TEN).longValue());
+        transaction.setAmountInCents(amount.multiply(BigDecimal.TEN).
+                multiply(BigDecimal.TEN).
+                longValue());
+
         transaction.setResultExchangeAmountInCents(longResultInCents);
         transaction.setTime(new Timestamp(System.currentTimeMillis()));
         transactionRepository.save(transaction);
 
         user.addTransaction(transaction);
+
+        logger.info(transactionMarker, "from " + amount + " " + from
+                + " to " + resultAmount + " " + to + " | rate " + rate);
+
         userRepository.save(user);
 
         List<Transaction> transactions = user.getTransactions();
-        return new ResponseEntity<>(new ExchangeResponse(transactions.get(transactions.size() - 1).getId(), resultAmount.toString()), HttpStatus.OK);
+        return new ResponseEntity<>(new
+
+                ExchangeResponse(transactions.get(transactions.size() - 1).
+
+                getId(), resultAmount.
+
+                toString()), HttpStatus.OK);
 
     }
 }
